a0052e1a6e6b3f6414d662d158bec9e30892f930
Alpha9304 <gbemisoladosu@gmail.com>
2023-09-14 23:50:11 -0400

    added to ViewPosts

--- Screens/ViewPosts.js
+++ Screens/ViewPosts.js
@@ -1,25 +1,31 @@
-//have a few posts
-//be able to add posts
-//want like button, comment button?, name (or anon), outfit style as rated by the analyzer and comments to be displayed in box(it's a box for now)
-import React from 'react';
+//need to be able to add post and description here
+//and choose by to be anon or get name from profile
+//get style from analysis (put in file name ig when saved?)
+//need to be able to post from Post or from Image Analyzer
+//need to be able to navigate to this screen after posting
+//need to be able to see only your posts (on profile or here)
+//increment likes on user press and with new post add new liked state
+//can restyle text and stuff
+
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
-  SafeAreaView,
   ScrollView,
   StatusBar, View, Image
 } from 'react-native';
 
 import {LinearGradient} from 'expo-linear-gradient';
-import LikeButton from '../components/Button'
+import { Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 
-const casualImageEx = require('../assets/images/casual_ex.jpg');
-const profImageEx = require('../assets/images/assets/prof_ex.jpg');
-const exerImageEx = require('../assets/images/exer_ex.jpg');
-const partyImageEx = require('../assets/images/party_ex.jpg');
+const casualImageEx = require('../images/assets/casual_ex.jpg');
+const profImageEx = require('../images/assets/prof_ex.jpg');
+const exerImageEx = require('../images/assets/exer_ex.jpg');
+const partyImageEx = require('../images/assets/party_ex.jpg');
 
-const App = () => {
+const ViewPosts = () => {
   
   function getaspectratio(img) {
     const data = image.resolveassetsource(img);
@@ -27,7 +33,10 @@ const App = () => {
   }
 
   
-
+  const [liked1, setLiked1] = useState(false);
+  const [liked2, setLiked2] = useState(false);
+  const [liked3, setLiked3] = useState(false);
+  const [liked4, setLiked4] = useState(false);
   
 
   return (
@@ -41,29 +50,147 @@ const App = () => {
           <View style={styles.imageContainer}>
             <Image source={casualImageEx} style={styles.image} />
           </View>
+
+
+          <View style={styles.interactContainer}>
+            <View style = {[styles.leftTextContainer, styles.textUnderline]}>
+              <Text style= {styles.text}>Outfit By: Amy</Text>
+            </View>
+
+            <View style = {styles.leftTextContainer}>
+              <Text style= {styles.text}>Style: Casual</Text>
+            </View>
+
+            <View style = {styles.descTextContainer}>
+              <Text style= {styles.descText}>Outfit for today!</Text>
+            </View>
+            
+            <View style = {styles.likeContainer}>
+              <Pressable onPress={() => setLiked1((isLiked) => !isLiked)}>
+                <Ionicons
+                  name={liked1 ? "heart" : "heart-outline"}
+                  size={32}
+                  color={liked1 ? '#fc3fc4' : "black"}
+                />
+              </Pressable>
+
+              <Text style = {styles.likeText}>313</Text>
+
+            </View>
+
+          </View>
         </View>
 
         <View style={styles.container}>
-          <View style={styles.interactContainer}>
-            <LikeButton></LikeButton>
-          </View>
+          
         </View>
 
         <View style={styles.container}>
           <View style={styles.imageContainer}>
             <Image source={profImageEx} style={styles.image} />
           </View>
+
+
+          <View style={styles.interactContainer}>
+            <View style = {[styles.leftTextContainer, styles.textUnderline]}>
+              <Text style= {styles.text}>Outfit By: Jeremy</Text>
+            </View>
+
+            <View style = {styles.leftTextContainer}>
+              <Text style= {styles.text}>Style: Professional</Text>
+            </View>
+
+            <View style = {styles.descTextContainer}>
+              <Text style= {styles.descText}>Ready to present.</Text>
+            </View>
+            
+            <View style = {styles.likeContainer}>
+              <Pressable onPress={() => setLiked2((isLiked) => !isLiked)}>
+                <Ionicons
+                  name={liked2 ? "heart" : "heart-outline"}
+                  size={32}
+                  color={liked2 ? '#fc3fc4' : "black"}
+                />
+              </Pressable>
+
+              <Text style = {styles.likeText}>200</Text>
+            </View>
+
+          </View>
+
+          
         </View>
 
         <View style={styles.container}>
           <View style={styles.imageContainer}>
             <Image source={exerImageEx} style={styles.image} />
           </View>
+
+
+          <View style={styles.interactContainer}>
+            <View style = {[styles.leftTextContainer, styles.textUnderline]}>
+              <Text style= {styles.text}>Outfit By: Lucy</Text>
+            </View>
+
+            <View style = {styles.leftTextContainer}>
+              <Text style= {styles.text}>Style: Exercise</Text>
+            </View>
+
+            <View style = {styles.descTextContainer}>
+              <Text style= {styles.descText}>Workout fit!!</Text>
+            </View>
+            
+            <View style = {styles.likeContainer}>
+              <Pressable onPress={() => setLiked3((isLiked) => !isLiked)}>
+                <Ionicons
+                  name={liked3 ? "heart" : "heart-outline"}
+                  size={32}
+                  color={liked3 ? '#fc3fc4' : "black"}
+                />
+              </Pressable>
+
+              <Text style = {styles.likeText}>114</Text>
+            </View>
+
+
+          </View>
+
+
+
         </View>
 
         <View style={styles.container}>
           <View style={styles.imageContainer}>
             <Image source={partyImageEx} style={styles.image} />
+          </View>
+
+
+
+          <View style={styles.interactContainer}>
+            <View style = {[styles.leftTextContainer, styles.textUnderline]}>
+              <Text style= {styles.text}>Outfit By: Mary</Text>
+            </View>
+
+            <View style = {styles.leftTextContainer}>
+              <Text style= {styles.text}>Style: Party</Text>
+            </View>
+
+            <View style = {styles.descTextContainer}>
+              <Text style= {styles.descText}>Ready for the dance!</Text>
+            </View>
+            
+            <View style = {styles.likeContainer}>
+              <Pressable onPress={() => setLiked4((isLiked) => !isLiked)}>
+                <Ionicons
+                  name={liked4 ? "heart" : "heart-outline"}
+                  size={32}
+                  color={liked4 ? '#fc3fc4' : "black"}
+                />
+              </Pressable>
+
+              <Text style = {styles.likeText}>350</Text>
+            </View>
+
           </View>
         </View>
 
@@ -82,9 +209,6 @@ const styles = StyleSheet.create({
   scrollView: {
     marginHorizontal: 0,
   },
-  text: {
-    fontSize: 42,
-  },
   image: {
     width: 350,
     height: 550,
@@ -92,7 +216,11 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-    paddingTop: 58,
+    borderWidth: 6,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
   },
   container: {
     flex: 1,
@@ -102,13 +230,60 @@ const styles = StyleSheet.create({
   }, 
   interactContainer: {
     flex: 1,
-    paddingTop: 58,
+    paddingTop: 10,
+    paddingLeft: 5,
     backgroundColor: 'white',
-    width: 350,
-    height: 250,
+    width: 355,
+    height: 150,
     borderRadius: 10,
-    alignItems: 'center',
+    paddingEnd: 10,
+    borderWidth: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
+  text: {
+    fontSize: 20,
+    fontFamily: 'Helvetica',
+    lineHeight: 32,
+    fontWeight: '300',
+    textAlign: 'left',
+    color: 'black',
+    borderRadius: 12,
+    position: 'absolute'
+  }, 
+  textUnderline: {
+    textDecorationLine: 'underline',
+    textDecoration: 'underline',
+    textDecorationStyle: 'solid',
+    textDecorationColor: 'black'
+  },
+  leftTextContainer: {
+    paddingBottom: 25,
+    justifyContent: 'flex-start',
+    borderWidth: 0,
+  },
+  descTextContainer: {
+    paddingBottom: 25,
+    paddingTop: 20,
+    justifyContent: 'flex-end',
+    borderWidth: 0,
+    fontSize: 60,
+  },
+  descText: {
+    fontSize: 25,
+  },
+  likeContainer: {
+    position: 'absolute',
+    paddingLeft: 290,
+    paddingTop: 15,
+  },
+  likeText: {
+    fontSize: 15,
+    color: '#fc3fc4',
+    paddingLeft: 5,
+  }
 });
 
-export default App;
+export default ViewPosts;
